eb24f65c8ead1c8b7a7581b5f93d07f438902556
Konstantin.Sch <57962038+KostiantynShcherbyna@users.noreply.github.com>
2023-10-14 10:25:51 +0300

    feat: prisma and neon connection works

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import {Module} from '@nestjs/common';
+import {PostController} from "./features/posts/api/post.controller";
+import {UserController} from "./features/users/api/user.controller";
+import {PostService} from "./features/posts/app/post.service";
+import {UserService} from "./features/users/app/user.service";
+import {PrismaService} from "./prisma.service";
 
 @Module({
     imports: [],
-    controllers: [],
-    providers: [],
+    controllers: [
+        PostController,
+        UserController,
+    ],
+    providers: [
+        PrismaService,
+        PostService,
+        UserService,
+    ],
 })
 export class AppModule {
 }
