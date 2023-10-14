03b86491f3d55f059a4717a948116ae465195b9c
Konstantin.Sch <57962038+KostiantynShcherbyna@users.noreply.github.com>
2023-10-14 13:41:22 +0300

    fix: fix spec test for deploy

--- apps/main/src/app.controller.spec.ts
+++ apps/main/src/app.controller.spec.ts
@@ -1,22 +1,26 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Test, TestingModule} from '@nestjs/testing';
+import {UserController} from "./features/users/api/user.controller";
+import {UserService} from "./features/users/app/user.service";
+import {PostController} from "./features/posts/api/post.controller";
+import {PostService} from "./features/posts/app/post.service";
+import {PrismaService} from "./prisma.service";
 
-describe('AppController', () => {
-  let appController: AppController;
 
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
-    }).compile();
+describe('PostController', () => {
+    let postController: PostController;
 
-    appController = app.get<AppController>(AppController);
-  });
+    beforeEach(async () => {
+        const app: TestingModule = await Test.createTestingModule({
+            controllers: [PostController],
+            providers: [PostService, PrismaService],
+        }).compile();
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+        postController = app.get<PostController>(PostController);
     });
-  });
+
+    describe('root', () => {
+        it('should return "Hello World!"', async () => {
+            expect(await postController.getPublishedPosts()).toBeDefined();
+        });
+    });
 });
