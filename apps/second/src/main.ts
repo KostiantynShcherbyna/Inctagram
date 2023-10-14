81295b610ddf1a0c4395b0fa4c0cd7fce015ab07
Konstantin.Sch <57962038+KostiantynShcherbyna@users.noreply.github.com>
2023-10-14 11:34:57 +0300

    fix: delete second mkserv

--- apps/second/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { MonorepoModule } from './monorepo.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(MonorepoModule);
-  await app.listen(3000);
-}
-bootstrap();
