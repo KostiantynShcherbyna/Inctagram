cbda58dec478c002a02d13d4ae5075ec5064568f
Konstantin.Sch <57962038+KostiantynShcherbyna@users.noreply.github.com>
2023-10-16 12:23:19 +0300

    refactor: fix code alignment

--- apps/main/src/app.controller.spec.ts
+++ apps/main/src/app.controller.spec.ts
@@ -1,26 +1,25 @@
-import {Test, TestingModule} from '@nestjs/testing';
-import {UserController} from "./features/users/api/user.controller";
-import {UserService} from "./features/users/app/user.service";
-import {PostController} from "./features/posts/api/post.controller";
-import {PostService} from "./features/posts/app/post.service";
-import {PrismaService} from "./prisma.service";
-
+import { Test, TestingModule } from '@nestjs/testing'
+import { UserController } from './features/users/api/user.controller'
+import { UserService } from './features/users/app/user.service'
+import { PostController } from './features/posts/api/post.controller'
+import { PostService } from './features/posts/app/post.service'
+import { PrismaService } from './prisma.service'
 
 describe('PostController', () => {
-    let postController: PostController;
+	let postController: PostController
 
-    beforeEach(async () => {
-        const app: TestingModule = await Test.createTestingModule({
-            controllers: [PostController],
-            providers: [PostService, PrismaService],
-        }).compile();
+	beforeEach(async () => {
+		const app: TestingModule = await Test.createTestingModule({
+			controllers: [PostController],
+			providers: [PostService, PrismaService]
+		}).compile()
 
-        postController = app.get<PostController>(PostController);
-    });
+		postController = app.get<PostController>(PostController)
+	})
 
-    describe('root', () => {
-        it('should return "Hello World!"', async () => {
-            expect(await postController.getPublishedPosts()).toBeDefined();
-        });
-    });
-});
+	describe('root', () => {
+		it('should return "Hello World!"', async () => {
+			expect(await postController.getPublishedPosts()).toBeDefined()
+		})
+	})
+})
--- apps/main/src/app.module.ts
+++ apps/main/src/app.module.ts
@@ -1,21 +1,13 @@
-import {Module} from '@nestjs/common';
-import {PostController} from "./features/posts/api/post.controller";
-import {UserController} from "./features/users/api/user.controller";
-import {PostService} from "./features/posts/app/post.service";
-import {UserService} from "./features/users/app/user.service";
-import {PrismaService} from "./prisma.service";
+import { Module } from '@nestjs/common'
+import { PostController } from './features/posts/api/post.controller'
+import { UserController } from './features/users/api/user.controller'
+import { PostService } from './features/posts/app/post.service'
+import { UserService } from './features/users/app/user.service'
+import { PrismaService } from './prisma.service'
 
 @Module({
-    imports: [],
-    controllers: [
-        PostController,
-        UserController,
-    ],
-    providers: [
-        PrismaService,
-        PostService,
-        UserService,
-    ],
+	imports: [],
+	controllers: [PostController, UserController],
+	providers: [PrismaService, PostService, UserService]
 })
-export class AppModule {
-}
+export class AppModule {}
