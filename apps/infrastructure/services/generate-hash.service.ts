9828146c9d008d0c34a0613c3d003787e7db4580
Konstantin.Sch <57962038+KostiantynShcherbyna@users.noreply.github.com>
2023-10-20 22:35:37 +0300

    feat: working on login.use-case.ts

--- apps/infrastructure/services/generate-hash.service.ts
+++ apps/infrastructure/services/generate-hash.service.ts
@@ -1,6 +1,6 @@
-import bcrypt from 'bcrypt'
+import * as bcrypt from 'bcrypt';
 
 export const generateHashService = async (dto: string) => {
-	const salt = await bcrypt.genSalt()
-	return bcrypt.hash(dto, salt)
+	// const salt = await bcrypt.genSalt()
+	return bcrypt.hash(dto, 10)
 }
