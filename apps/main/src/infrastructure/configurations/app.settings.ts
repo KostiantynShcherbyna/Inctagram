43e9686dd729de8bd225d17812ac57b7af2dae34
Konstantin.Sch <57962038+KostiantynShcherbyna@users.noreply.github.com>
2023-10-23 09:48:52 +0300

    fix: app settings

--- apps/main/src/infrastructure/configurations/app.settings.ts
+++ apps/main/src/infrastructure/configurations/app.settings.ts
@@ -4,6 +4,8 @@ import { ErrorExceptionFilter, HttpExceptionFilter } from '../utils/exeption-fil
 import { AppModule } from '../../app.module'
 import { errorMessagesAdapter } from '../adapters/error-messages.adapter'
 import * as cookieParser from 'cookie-parser'
+import * as session from 'express-session'
+import * as passport from 'passport'
 
 export const appSettings = (app: INestApplication) => {
 	app.use(cookieParser())
@@ -19,6 +21,19 @@ export const appSettings = (app: INestApplication) => {
 		new ErrorExceptionFilter(),
 		new HttpExceptionFilter()
 	)
+	app.setGlobalPrefix('api')
+	app.use(
+		session({
+			secret: 'dhwye08u4w90ri0w94ur09wi3-0',
+			saveUninitialized: false,
+			resave: false,
+			cookie: {
+				maxAge: 60000
+			}
+		})
+	)
+	app.use(passport.initialize())
+	app.use(passport.session())
 	useContainer(app.select(AppModule), { fallbackOnErrors: true })
 	return app
 }
