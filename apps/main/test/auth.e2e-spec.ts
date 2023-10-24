431df7b7331a0265babf5237a67a628ba9744b2d
Konstantin.Sch <57962038+KostiantynShcherbyna@users.noreply.github.com>
2023-10-24 12:39:11 +0300

    fix: fix e2e

--- apps/main/test/auth.e2e-spec.ts
+++ apps/main/test/auth.e2e-spec.ts
@@ -10,6 +10,7 @@ import { appSettings } from '../src/infrastructure/configurations/app.settings'
 import { faker } from '@faker-js/faker'
 import { endpoints } from './infrastructure/routing.helper'
 import { RegistrationBodyInputModel } from '../src/features/auth/utils/models/input/registration.body.input-model'
+import { LOGIN_MAX_LENGTH, LOGIN_MIN_LENGTH } from '../src/infrastructure/utils/constants'
 
 describe
 ('Auth', () => {
@@ -56,7 +57,9 @@ describe
 		let inputDataUser_0: RegistrationBodyInputModel
 		it(`+ registration user_0`, async () => {
 			inputDataUser_0 = {
-				login: faker.person.firstName(),
+				login: faker.lorem.word({
+					length: { min: LOGIN_MIN_LENGTH, max: LOGIN_MAX_LENGTH }
+				}),
 				email: faker.internet.email(),
 				password: faker.internet.password()
 			}
