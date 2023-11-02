1618a6e19910a8ef02091fc8e04dadc769a8ac95
Konstantin.Sch <57962038+KostiantynShcherbyna@users.noreply.github.com>
2023-11-02 22:08:51 +0200

    fix: rollback corse

--- apps/main/src/infrastructure/settings/app.settings.ts
+++ apps/main/src/infrastructure/settings/app.settings.ts
@@ -11,9 +11,7 @@ import { firebaseConfig } from './firebase.settings'
 
 export const appSettings = (app: INestApplication) => {
 	app.use(cookieParser())
-	app.enableCors({
-		origin: ['http://localhost:3000/', 'localhost', 'localhost:3000', 'http://localhost:3000']
-	})
+	app.enableCors()
 	app.useGlobalPipes(
 		new ValidationPipe({
 			transform: true,
