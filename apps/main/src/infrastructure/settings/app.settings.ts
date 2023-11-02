453e47134d2a22107c0154ed2ab6e73a9fc76234
Konstantin.Sch <57962038+KostiantynShcherbyna@users.noreply.github.com>
2023-11-02 19:24:52 +0200

    fix: corses

--- apps/main/src/infrastructure/settings/app.settings.ts
+++ apps/main/src/infrastructure/settings/app.settings.ts
@@ -11,6 +11,9 @@ import { firebaseConfig } from './firebase.settings'
 
 export const appSettings = (app: INestApplication) => {
 	app.use(cookieParser())
+	app.enableCors({
+		origin: false
+	})
 	// app.enableCors({
 	// 	origin: ['http://localhost:3000', 'http://localhost:3000/api', 'https://visualvoyage.ru', 'https://visualvoyage.ru/api']
 	// })
