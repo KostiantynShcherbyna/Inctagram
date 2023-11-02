0e556de12956973733c8be693b5b38879894b416
Konstantin.Sch <57962038+KostiantynShcherbyna@users.noreply.github.com>
2023-11-02 17:13:48 +0200

    fix: upload and delete avatar 2

--- apps/main/src/features/users/app/use-cases/delete-avatar.use-case.ts
+++ apps/main/src/features/users/app/use-cases/delete-avatar.use-case.ts
@@ -1,7 +1,7 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs'
 import { ErrorEnum } from '../../../../infrastructure/utils/error-enum'
 import { FilesFirebaseAdapter } from '../../../../infrastructure/adapters/files.firebase.adapter'
-import { PrismaClient } from '@prisma/client'
+import { Avatar, PrismaClient } from '@prisma/client'
 
 export class DeleteAvatarCommand {
 	constructor(public userId: string) {
@@ -18,15 +18,18 @@ export class DeleteAvatarUseCase implements ICommandHandler<DeleteAvatarCommand>
 	}
 
 	async execute(command: DeleteAvatarCommand) {
-		const avatar = await this.prismaClient.avatar
-			.findFirst({ where: { userId: command.userId, active: true } })
-
+		const avatar = await this.prismaClient.avatar.findFirst(
+			{ where: { userId: command.userId, active: true } })
 		if (!avatar) return ErrorEnum.NOT_FOUND
-
-		await this.filesFirebaseAdapter.deleteAvatar(avatar.uploadPath)
-		await this.prismaClient.avatar.delete({ where: { id: avatar.id } })
+		await this.deleteAvatar(avatar)
 		return true
 	}
 
+	private async deleteAvatar(avatar: Avatar) {
+		return this.prismaClient.$transaction(async (tx) => {
+			await tx.avatar.delete({ where: { id: avatar.id } })
+			await this.filesFirebaseAdapter.deleteAvatar(avatar.uploadPath)
+		})
+	}
 
 }
