ed3a63afffab1372cc79f136cc9dbb0a6ca97174
Konstantin.Sch <57962038+KostiantynShcherbyna@users.noreply.github.com>
2023-11-03 16:28:05 +0200

    fix: try corse

--- apps/main/src/infrastructure/settings/app.settings.ts
+++ apps/main/src/infrastructure/settings/app.settings.ts
@@ -13,15 +13,10 @@ export const appSettings = (app: INestApplication) => {
 	app.use(cookieParser())
 	app.enableCors({
 		origin: 'http://localhost:3000', // Замените это на URL вашего клиентского приложения
-		methods: ['GET', 'PUT', 'POST', 'DELETE', 'HEAD', 'PATCH'],
+		methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+		allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
 		credentials: true
 	})
-	// app.enableCors({
-	// 	origin: ['http://localhost:3000'],
-	// 	methods: ['GET', 'PUT', 'POST', 'DELETE'],
-	// 	allowedHeaders: ['Content-Type', 'Authorization'],
-	// 	credentials: true
-	// })
 	app.useGlobalPipes(
 		new ValidationPipe({
 			transform: true,
