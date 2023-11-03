9827bf5113af6740bd43195264bd3f9f6927594d
Konstantin.Sch <57962038+KostiantynShcherbyna@users.noreply.github.com>
2023-11-03 15:43:44 +0200

    fix: try corse

--- apps/main/src/infrastructure/settings/app.settings.ts
+++ apps/main/src/infrastructure/settings/app.settings.ts
@@ -11,18 +11,12 @@ import { firebaseConfig } from './firebase.settings'
 
 export const appSettings = (app: INestApplication) => {
 	app.use(cookieParser())
-
 	app.use((req, res, next) => {
-		res.header('Access-Control-Allow-Origin', '*')
-		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-		res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-		next()
-	})
-
-	app.enableCors({
-		allowedHeaders: '*',
-		origin: '*'
-	})
+		res.header('Access-Control-Allow-Origin', '*');
+		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+		res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+		next();
+	});
 	app.useGlobalPipes(
 		new ValidationPipe({
 			transform: true,
