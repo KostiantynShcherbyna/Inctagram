4ff3be6687053fc28c8622a5ad9789394c366960
Konstantin.Sch <57962038+KostiantynShcherbyna@users.noreply.github.com>
2023-11-04 18:36:58 +0200

    fix: flobalprefix and try kuber

--- apps/main/src/infrastructure/utils/exception-filter.ts
+++ apps/main/src/infrastructure/utils/exception-filter.ts
@@ -32,7 +32,6 @@ export class HttpExceptionFilter implements ExceptionFilter {
 				: response.sendStatus(status)
 		}
 
-
 		// ↓↓↓ UNAUTHORIZED
 		// if (status === HttpStatus.UNAUTHORIZED) {
 		// 	const errorsMessages = this.messagesModify(exceptionResponse)
@@ -41,14 +40,14 @@ export class HttpExceptionFilter implements ExceptionFilter {
 		// 		? response.status(status).send({ errorsMessages })
 		// 		: response.sendStatus(status)
 		// }
-		// // ↓↓↓ NOT_FOUND
-		// if (status === HttpStatus.NOT_FOUND) {
-		// 	const errorsMessages = this.messagesModify(exceptionResponse)
-		//
-		// 	return errorsMessages
-		// 		? response.status(status).send({ errorsMessages })
-		// 		: response.sendStatus(status)
-		// }
+		// ↓↓↓ NOT_FOUND
+		if (status === HttpStatus.NOT_FOUND) {
+			const errorsMessages = this.messagesModify(exceptionResponse)
+
+			return errorsMessages
+				? response.status(status).send({ errorsMessages })
+				: response.sendStatus(status)
+		}
 
 		return response.status(status).send(exceptionResponse)
 	}
