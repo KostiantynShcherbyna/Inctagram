6f5e4af0d9057a771dc3b33558d5bab1042924cb
Konstantin.Sch <57962038+KostiantynShcherbyna@users.noreply.github.com>
2023-11-07 20:50:31 +0200

    fix: confiramtion

--- apps/main/src/features/auth/app/use-cases/email-confirmation.use-case.ts
+++ apps/main/src/features/auth/app/use-cases/email-confirmation.use-case.ts
@@ -1,6 +1,5 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs'
 import { UsersRepository } from '../../../users/rep/users.repository'
-import { ReturnContract } from '../../../../infrastructure/utils/return-contract'
 import { ErrorEnum } from '../../../../infrastructure/utils/error-enum'
 import { TokensService } from '../../../../infrastructure/services/tokens.service'
 import { ConfigService } from '@nestjs/config'
@@ -27,28 +26,24 @@ export class EmailConfirmationUseCase
 		const confirmationCode = await this.usersRepository
 			.findConfirmationCode(command.code)
 
-		if (confirmationCode === null)
-			return new ReturnContract(null, ErrorEnum.CONFIRMATION_CODE_NOT_FOUND)
+		if (confirmationCode === null) return ErrorEnum.CONFIRMATION_CODE_NOT_FOUND
 
 		const user = await this.usersRepository.findUserById(confirmationCode.userId)
 
 		if (user === null)
-			return new ReturnContract(null, ErrorEnum.USER_NOT_FOUND)
-		if (user.isConfirmed === true)
-			return new ReturnContract(null, ErrorEnum.EMAIL_CONFIRMED)
+			return ErrorEnum.USER_NOT_FOUND
+		if (user.isConfirmed === true) return ErrorEnum.EMAIL_CONFIRMED
 
 		const confirmationCodeDto = await this.tokensService
 			.verifyToken(command.code, env.EMAIL_CONFIRMATION_CODE_SECRET)
 
-		if (confirmationCodeDto === null)
-			return new ReturnContract(null, ErrorEnum.INVALID_TOKEN)
+		if (confirmationCodeDto === null) return ErrorEnum.INVALID_TOKEN
 
 		const updateResult = await this.usersRepository
 			.updateConfirmation(user.id, true)
 
-		if (!updateResult)
-			return new ReturnContract(null, ErrorEnum.USER_NOT_FOUND)
+		if (!updateResult) return ErrorEnum.USER_NOT_FOUND
 
-		return new ReturnContract(true, null)
+		return
 	}
 }
