d46fa241b3384dc52c9954bafb700675399f930d
Konstantin.Sch <57962038+KostiantynShcherbyna@users.noreply.github.com>
2023-11-11 18:59:25 +0200

    fix: cookie

--- apps/main/src/infrastructure/settings/app.settings.ts
+++ apps/main/src/infrastructure/settings/app.settings.ts
@@ -12,7 +12,7 @@ import { firebaseConfig } from './firebase.settings'
 export const appSettings = (app: INestApplication) => {
 	app.use(cookieParser())
 	app.enableCors({
-		origin: ['http://localhost:3000', 'https://visualvoyage.ru', 'https://inctagram-tau.vercel.app'],
+		origin: '*',
 		credentials: true
 	})
 	app.setGlobalPrefix('api/v1')
